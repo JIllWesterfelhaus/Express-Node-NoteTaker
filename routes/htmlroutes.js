ef2e7ae3c7e049346944cca48bcee5fe361ea503
JillWesterfelhaus <jillwesthaus@gmail.com>
2020-04-29 10:04:07 -0500

    trying to get notes to update in html

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -1,6 +1,6 @@
 var path = require("path")
 module.exports = function(app) {
-//need to create notes.html file
+//need to create notes.html file 
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
   });
@@ -9,3 +9,4 @@ app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 }
+
